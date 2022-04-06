4e039590b24d8fafc62416395ba3fcfa3d0e1d04
Aynurbalci <64687379+Aynurbalci@users.noreply.github.com>
2022-04-06 03:57:32 +0300

    templates

---
+++ resources/views/Home/_header.blade.php
@@ -0,0 +1,96 @@
+  <!-- =============== Preloader =============== -->
+  <div id="preloader">
+        <div id="loading">
+            <img width="256" height="32" src="{{ asset('assets')}}/img/loading-cylon-red.svg">
+        </div>
+    </div>
+    <!-- =============== nav =============== -->
+    <nav id="mainNav" class="navbar navbar-default navbar-fixed-top">
+        <div class="container">
+            <div class="container-fluid">
+                <!-- Brand and toggle get grouped for better mobile display -->
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                        <span class="sr-only">Toggle navigation</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                    <a class="navbar-brand" href="#"><img src="assets/img/logo.png" alt="Logo">
+                    </a>
+                </div>
+
+                <!-- Collect the nav links, forms, and other content for toggling -->
+
+                <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                    <ul class="nav navbar-nav navbar-right">
+                        <li class="page-scroll" > <a href="#home">Home</a>
+                        <ul class="page-scroll">
+										<li><a href="">Static Home</a></li>
+										<li><a href="">Slider Home</a></li>
+									</ul>
+
+                        </li>
+
+                        <li>
+                            <a class="page-scroll" href="#about">About</a>
+                        </li>
+                        <li>
+                            <a class="page-scroll" href="#Services">Services</a>
+                        </li>
+                        <li>
+                            <a class="page-scroll" href="#Gallery">Gallery</a>
+                        </li>
+                        <li>
+                            <a class="page-scroll" href="#blog">Blog</a>
+                        </li>
+                        <li>
+                            <a class="page-scroll" href="#contact">Contact</a>
+                        </li>
+                    </ul>
+                </div>
+                <!-- =============== navbar-collapse =============== -->
+
+            </div>
+        </div>
+        <!-- =============== container-fluid =============== -->
+    </nav>
+    <!-- =============== header =============== -->
+    <header id="home" class="header">
+        <!-- =============== container =============== -->
+        <div class="container">
+            <div class="header-content row">
+
+
+                <div id="owl-demo" class="owl-carousel header1">
+                    <div>
+                        <div class="col-xs-12 col-sm-6 col-md-6 header-text">
+                            <h2 class="wow bounceIn animated" data-wow-delay=".40s">FRIENDLY <span>TEMPLATE</span> FOR <br/>YOUR WEBSITE</h2>
+                            <h3 class="wow bounceIn animated" data-wow-delay=".50s">FOR YOUR WEBSITE </h3>
+                            <p class="wow bounceIn animated" data-wow-delay=".60s">Lorem ipsum dolor sit amet, consectetur adipiscing elit.We ensure sit amet, consectetur adipiscing elit</p>
+                            <p>
+                                <div class="btn btn-primary btn-lg btn-ornge wow bounceIn animated" data-wow-delay="1s"><i class="hbtn"></i> <span>Click Here</span></i>
+                                </div>
+                            </p>
+                        </div>
+                    </div>
+                    <div>
+                        <div class="col-xs-12 col-sm-6 col-md-6 header-text">
+                            <h2 class="wow bounceIn animated" data-wow-delay=".40s">FRIENDLY <span>THIS</span> FOR <br/>YOUR WEBSITE</h2>
+                            <h3 class="wow bounceIn animated" data-wow-delay=".50s">FOR YOUR WEBSITE </h3>
+                            <p class="wow bounceIn animated" data-wow-delay=".60s">Lorem ipsum dolor sit amet, consectetur adipiscing elit.We ensure sit amet, consectetur adipiscing elit</p>
+                            <p>
+                                <div class="btn btn-primary btn-lg btn-ornge wow bounceIn animated" data-wow-delay="1s"><i class="hbtn"></i> <span>Click Here</span></i>
+                                </div>
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+
+
+        </div>
+        <!-- =============== container end =============== -->
+    </header>
+
--- resources/views/layouts/home.blade.php
+++ resources/views/layouts/home.blade.php
@@ -1,44 +1,52 @@
 <!DOCTYPE html>
+<!--
+	Be by TEMPLATE STOCK
+	templatestock.co @templatestock
+	Released for free under the Creative Commons Attribution 3.0 license (templated.co/license)
+-->
 <html lang="en">
+
 <head>
-  <title>Bootstrap 5 Example</title>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+
+    <title>Be Free HTML5 Responsive Template | Template Stock</title>
+
+    <!-- =============== HTML Meta Tags =============== -->
+
+    <meta name="description" content="Free Web tutorials">
+    <meta name="keywords" content="HTML, CSS, JavaScript">
+    <meta name="author" content="Aynur BALCI">
+
+    <!-- =============== Bootstrap Core CSS =============== -->
+    <link rel="stylesheet" href="{{ asset('assets')}}/css/bootstrap.min.css" type="text/css">
+    <!-- =============== fonts awesome =============== -->
+    <link rel="stylesheet" href="{{ asset('assets')}}/font/css/font-awesome.min.css" type="text/css">
+    <!-- =============== Plugin CSS =============== -->
+    <link rel="stylesheet" href="{{ asset('assets')}}/css/animate.min.css" type="text/css">
+    <!-- =============== Custom CSS =============== -->
+    <link rel="stylesheet" href="{{ asset('assets')}}/css/style.css" type="text/css">
+    <!-- =============== Owl Carousel Assets =============== -->
+    <link href="{{ asset('assets')}}/owl-carousel/owl.carousel.css" rel="stylesheet">
+    <link href="{{ asset('assets')}}/owl-carousel/owl.theme.css" rel="stylesheet">
+
+    <link rel="stylesheet" href="{{ asset('assets')}}/css/isotope-docs.css" media="screen">
+    <link rel="stylesheet" href="{{ asset('assets')}}/css/baguetteBox.css">
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
 </head>
+
 <body>
-
-<div class="container-fluid p-5 bg-primary text-white text-center">
-  <h1>Header</h1>
-  <p>Resize this responsive page to see the effect!</p>
-</div>
-
-<div class="container mt-5">
-  <div class="row">
-    <div class="col-sm-4">
-      <h3>Column 1</h3>
-      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit...</p>
-      <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris...</p>
-    </div>
-    <div class="col-sm-4">
-      <h3>Column 2</h3>
-      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit...</p>
-      <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris...</p>
-    </div>
-    <div class="col-sm-4">
-      <h3>Column 3</h3>
-      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit...</p>
-      <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris...</p>
-    </div>
-
-  </div>
-
-</div>
-<div class="container-fluid p-5 bg-primary text-white text-center">
-  <h1>Footer</h1>
-  <p>Resize this responsive page to see the effect!</p>
-</div>
+@include('home._header')
+@include('home._footer')
 
 </body>
+
 </html>
