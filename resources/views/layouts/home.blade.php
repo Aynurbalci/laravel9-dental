b78a3cae8862b34dc5e7c284d5e41c42db5fec07
Aynurbalci <64687379+Aynurbalci@users.noreply.github.com>
2022-04-07 01:58:04 +0300

    add contact

--- resources/views/layouts/home.blade.php
+++ resources/views/layouts/home.blade.php
@@ -7,6 +7,12 @@
         <meta content="eCommerce HTML Template Free Download" name="keywords">
         <meta content="eCommerce HTML Template Free Download" name="description">
 
+        <!-- HTML META ETİKETLERİ -->
+        <meta name="description" content="Free Web tutorials">
+  <meta name="keywords" content="HTML, CSS, JavaScript">
+  <meta name="author" content="Aynur BALCI">
+
+
         <!-- Favicon -->
         <link href="img/favicon.ico" rel="icon">
 
@@ -29,11 +35,61 @@
 
         <!-- Template Javascript -->
         <script src="{{ asset('assests')}}/js/main.js"></script>
+
+
+        <!-- 2 -->
+        <!-- =============== Bootstrap Core CSS =============== -->
+    <link rel="stylesheet" href="{{ asset('assests')}}/css/bootstrap.min.css" type="text/css">
+    <!-- =============== fonts awesome =============== -->
+    <link rel="stylesheet" href="{{ asset('assests')}}/font/css/font-awesome.min.css" type="text/css">
+    <!-- =============== Plugin CSS =============== -->
+    <link rel="stylesheet" href="{{ asset('assests')}}/css/animate.min.css" type="text/css">
+    <!-- =============== Custom CSS =============== -->
+    <link rel="stylesheet" href="{{ asset('assests')}}/css/style1.css" type="text/css">
+    <!-- =============== Owl Carousel Assets =============== -->
+    <link href="{{ asset('assests')}}/owl-carousel/owl.carousel.css" rel="stylesheet">
+    <link href="{{ asset('assests')}}/owl-carousel/owl.theme.css" rel="stylesheet">
+
+	 <link rel="stylesheet" href="assets{{ asset('assests')}}/css/isotope-docs.css" media="screen">
+	  <link rel="stylesheet" href="assets{{ asset('assests')}}/css/baguetteBox.css">
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+    <!-- =============== jQuery =============== -->
+    <script src="{{ asset('assests')}}/js/jquery.js"></script>
+	 <script src="{{ asset('assests')}}/js/isotope-docs.min.js"></script>
+    <!-- =============== Bootstrap Core JavaScript =============== -->
+    <script src="{{ asset('assests')}}/js/bootstrap.min.js"></script>
+    <!-- =============== Plugin JavaScript =============== -->
+    <script src="{{ asset('assests')}}/js/jquery.easing.min.js"></script>
+    <script src="{{ asset('assests')}}/js/jquery.fittext.js"></script>
+    <script src="{{ asset('assests')}}/js/wow.min.js"></script>
+	<!-- =============== owl carousel =============== -->
+    <script src="{{ asset('assests')}}/owl-carousel/owl.carousel.js"></script>
+	<!-- Isotope does NOT require jQuery. But it does make things easier -->
+
+<script src="{{ asset('assests')}}/js/baguetteBox.js" async></script>
+<script src="{{ asset('assests')}}/js/plugins.js" async></script>
+
+    <!-- =============== Custom Theme JavaScript =============== -->
+    <script src="{{ asset('assests')}}/js/creative.js">	</script>
+<script src="{{ asset('assests')}}/js/jquery.nicescroll.min.js"></script>
+
+
+
     </head>
 
     <body>
 
 @include('home._header')
+
+@include('home._contact')
+
+
+@include('home._footer')
 
 
 </body>
