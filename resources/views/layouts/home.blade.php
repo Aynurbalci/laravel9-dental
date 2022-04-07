6b0173226842039ba0590a81a94060532b82afb0
Aynurbalci <64687379+Aynurbalci@users.noreply.github.com>
2022-04-08 01:27:04 +0300

    operation of slider

--- resources/views/layouts/home.blade.php
+++ resources/views/layouts/home.blade.php
@@ -1,44 +1,45 @@
 <!DOCTYPE html>
 <html lang="en">
-    <head>
-        <meta charset="utf-8">
-        <title>E Store - eCommerce HTML Template</title>
-        <meta content="width=device-width, initial-scale=1.0" name="viewport">
-        <meta content="eCommerce HTML Template Free Download" name="keywords">
-        <meta content="eCommerce HTML Template Free Download" name="description">
 
-        <!-- HTML META ETİKETLERİ -->
-        <meta name="description" content="Free Web tutorials">
-  <meta name="keywords" content="HTML, CSS, JavaScript">
-  <meta name="author" content="Aynur BALCI">
+<head>
+    <meta charset="utf-8">
+    <title>E Store - eCommerce HTML Template</title>
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+    <meta content="eCommerce HTML Template Free Download" name="keywords">
+    <meta content="eCommerce HTML Template Free Download" name="description">
 
-
-        <!-- Favicon -->
-        <link href="img/favicon.ico" rel="icon">
-
-        <!-- Google Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400|Source+Code+Pro:700,900&display=swap" rel="stylesheet">
-
-        <!-- CSS Libraries -->
-        <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" rel="stylesheet">
-        <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.0/css/all.min.css" rel="stylesheet">
-        <link href="{{ asset('assests')}}/lib/slick/slick.css" rel="stylesheet">
-        <link href="{{ asset('assests')}}/lib/slick/slick-theme.css" rel="stylesheet">
-
-        <!-- Template Stylesheet -->
-        <link href="{{ asset('assests')}}/css/style.css" rel="stylesheet">
-         <!-- JavaScript Libraries -->
-        <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
-        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.bundle.min.js"></script>
-        <script src="{{ asset('assests')}}/lib/easing/easing.min.js"></script>
-        <script src="{{ asset('assests')}}/lib/slick/slick.min.js"></script>
-
-        <!-- Template Javascript -->
-        <script src="{{ asset('assests')}}/js/main.js"></script>
+    <!-- HTML META ETİKETLERİ -->
+    <meta name="description" content="Free Web tutorials">
+    <meta name="keywords" content="HTML, CSS, JavaScript">
+    <meta name="author" content="Aynur BALCI">
 
 
-        <!-- 2 -->
-        <!-- =============== Bootstrap Core CSS =============== -->
+    <!-- Favicon -->
+    <link href="img/favicon.ico" rel="icon">
+
+    <!-- Google Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400|Source+Code+Pro:700,900&display=swap" rel="stylesheet">
+
+    <!-- CSS Libraries -->
+    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" rel="stylesheet">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.0/css/all.min.css" rel="stylesheet">
+    <link href="{{ asset('assests')}}/lib/slick/slick.css" rel="stylesheet">
+    <link href="{{ asset('assests')}}/lib/slick/slick-theme.css" rel="stylesheet">
+
+    <!-- Template Stylesheet -->
+    <link href="{{ asset('assests')}}/css/style.css" rel="stylesheet">
+    <!-- JavaScript Libraries -->
+    <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.bundle.min.js"></script>
+    <script src="{{ asset('assests')}}/lib/easing/easing.min.js"></script>
+    <script src="{{ asset('assests')}}/lib/slick/slick.min.js"></script>
+
+    <!-- Template Javascript -->
+    <script src="{{ asset('assests')}}/js/main.js"></script>
+
+
+    <!-- 2 -->
+    <!-- =============== Bootstrap Core CSS =============== -->
     <link rel="stylesheet" href="{{ asset('assests')}}/css/bootstrap.min.css" type="text/css">
     <!-- =============== fonts awesome =============== -->
     <link rel="stylesheet" href="{{ asset('assests')}}/font/css/font-awesome.min.css" type="text/css">
@@ -50,8 +51,8 @@
     <link href="{{ asset('assests')}}/owl-carousel/owl.carousel.css" rel="stylesheet">
     <link href="{{ asset('assests')}}/owl-carousel/owl.theme.css" rel="stylesheet">
 
-	 <link rel="stylesheet" href="assets{{ asset('assests')}}/css/isotope-docs.css" media="screen">
-	  <link rel="stylesheet" href="assets{{ asset('assests')}}/css/baguetteBox.css">
+    <link rel="stylesheet" href="assets{{ asset('assests')}}/css/isotope-docs.css" media="screen">
+    <link rel="stylesheet" href="assets{{ asset('assests')}}/css/baguetteBox.css">
     <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
     <!--[if lt IE 9]>
@@ -60,36 +61,224 @@
     <![endif]-->
     <!-- =============== jQuery =============== -->
     <script src="{{ asset('assests')}}/js/jquery.js"></script>
-	 <script src="{{ asset('assests')}}/js/isotope-docs.min.js"></script>
+    <script src="{{ asset('assests')}}/js/isotope-docs.min.js"></script>
     <!-- =============== Bootstrap Core JavaScript =============== -->
     <script src="{{ asset('assests')}}/js/bootstrap.min.js"></script>
     <!-- =============== Plugin JavaScript =============== -->
     <script src="{{ asset('assests')}}/js/jquery.easing.min.js"></script>
     <script src="{{ asset('assests')}}/js/jquery.fittext.js"></script>
     <script src="{{ asset('assests')}}/js/wow.min.js"></script>
-	<!-- =============== owl carousel =============== -->
+    <!-- =============== owl carousel =============== -->
     <script src="{{ asset('assests')}}/owl-carousel/owl.carousel.js"></script>
-	<!-- Isotope does NOT require jQuery. But it does make things easier -->
+    <!-- Isotope does NOT require jQuery. But it does make things easier -->
 
-<script src="{{ asset('assests')}}/js/baguetteBox.js" async></script>
-<script src="{{ asset('assests')}}/js/plugins.js" async></script>
+    <script src="{{ asset('assests')}}/js/baguetteBox.js" async></script>
+    <script src="{{ asset('assests')}}/js/plugins.js" async></script>
 
     <!-- =============== Custom Theme JavaScript =============== -->
-    <script src="{{ asset('assests')}}/js/creative.js">	</script>
-<script src="{{ asset('assests')}}/js/jquery.nicescroll.min.js"></script>
+    <script src="{{ asset('assests')}}/js/creative.js"> </script>
+    <script src="{{ asset('assests')}}/js/jquery.nicescroll.min.js"></script>
+
+    <!-- nav-->
+    <link href="{{asset('assests')}}/css/style3.css" rel="stylesheet" type="text/css" />
+
+    <!-- 3-->
+    <!-- Favicon -->
+    <link href="{{ asset('assests')}}/img/favicon.ico" rel="icon">
+
+    <!-- Google Web Fonts -->
+    <link href="https://fonts.googleapis.com/css2?family=Nunito+Sans&family=Nunito:wght@600;700;800&display=swap" rel="stylesheet">
+
+    <!-- Font Awesome -->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.0/css/all.min.css" rel="stylesheet">
+
+    <!-- Flaticon Font -->
+    <link href="{{ asset('assests')}}/lib/flaticon/font/flaticon.css" rel="stylesheet">
+
+    <!-- Libraries Stylesheet -->
+    <link href="{{ asset('assests')}}/lib/owlcarousel/assets/owl.carousel.min.css" rel="stylesheet">
+    <link href="{{ asset('assests')}}/lib/tempusdominus/css/tempusdominus-bootstrap-4.min.css" rel="stylesheet" />
+
+    <!-- Customized Bootstrap Stylesheet -->
+    <link href="{{ asset('assests')}}/css/style4.css" rel="stylesheet">
+
+</head>
+
+<body>
+
+    @include('home._header')
+    <!-- Carousel Start -->
+
+    <!-- Main Slider Start -->
+
+    <div class="container-fluid p-0">
+        <div class="carousel-inner">
+            <div class="col-md-3">
+                <nav class="navbar bg-light">
+                    <ul class="navbar-nav">
+                        <li class="nav-item">
+                            <a class="nav-link" href="#"><i class="fa fa-home"></i>Home</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#"><i class="fa fa-shopping-bag"></i>Best Selling</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#"><i class="fa fa-plus-square"></i>New Arrivals</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#"><i class="fa fa-female"></i>Fashion & Beauty</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#"><i class="fa fa-child"></i>Kids & Babies Clothes</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#"><i class="fa fa-tshirt"></i>Men & Women Clothes</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#"><i class="fa fa-mobile-alt"></i>Gadgets & Accessories</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#"><i class="fa fa-microchip"></i>Electronics & Accessories</a>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+            <div class="col-md-7">
+                <div id="header-carousel" class="carousel slide" data-ride="carousel">
+                    <div class="carousel-inner">
+                        <div class="carousel-item active">
+                            <img class="w-100" src="{{ asset('assests')}}/img/carousel-1.jpg" alt="Image">
+                            <div class="carousel-caption d-flex flex-column align-items-center justify-content-center">
+                                <div class="p-3" style="max-width: 900px;">
+                                    <h3 class="text-white mb-3 d-none d-sm-block">Best Pet Services</h3>
+                                    <h1 class="display-3 text-white mb-3">Keep Your Pet Happy</h1>
+                                    <h5 class="text-white mb-3 d-none d-sm-block">Duo nonumy et dolor tempor no et. Diam sit diam sit diam erat</h5>
+                                    <a href="" class="btn btn-lg btn-primary mt-3 mt-md-4 px-4">Book Now</a>
+                                    <a href="" class="btn btn-lg btn-secondary mt-3 mt-md-4 px-4">Learn More</a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="carousel-item">
+                            <img class="w-100" src="{{ asset('assests')}}/img/carousel-2.jpg" alt="Image">
+                            <div class="carousel-caption d-flex flex-column align-items-center justify-content-center">
+                                <div class="p-3" style="max-width: 900px;">
+                                    <h3 class="text-white mb-3 d-none d-sm-block">Best Pet Services</h3>
+                                    <h1 class="display-3 text-white mb-3">Pet Spa & Grooming</h1>
+                                    <h5 class="text-white mb-3 d-none d-sm-block">Duo nonumy et dolor tempor no et. Diam sit diam sit diam erat</h5>
+                                    <a href="" class="btn btn-lg btn-primary mt-3 mt-md-4 px-4">Book Now</a>
+                                    <a href="" class="btn btn-lg btn-secondary mt-3 mt-md-4 px-4">Learn More</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <a class="carousel-control-prev" href="#header-carousel" data-slide="prev">
+                        <div class="btn btn-primary rounded" style="width: 45px; height: 45px;">
+                            <span class="carousel-control-prev-icon mb-n2"></span>
+                        </div>
+                    </a>
+                    <a class="carousel-control-next" href="#header-carousel" data-slide="next">
+                        <div class="btn btn-primary rounded" style="width: 45px; height: 45px;">
+                            <span class="carousel-control-next-icon mb-n2"></span>
+                        </div>
+                    </a>
+                </div>
+            </div>
 
 
+        </div>
+    </div>
+    <!-- Review Starsdt -->
+    <div class="container-fluid">
+        <div id="header-carousel" class="carousel slide" data-ride="carousel">
+            <div class="carousel-inner">
+                <div class="carousel-item active">
+                    <img class="w-100" src="{{ asset('assests')}}/img/carousel-1.jpg" alt="Image">
+                    <div class="carousel-caption d-flex flex-column align-items-center justify-content-center">
+                        <div class="p-3" style="max-width: 900px;">
+                            <h3 class="text-white mb-3 d-none d-sm-block">Best Pet Services</h3>
+                            <h1 class="display-3 text-white mb-3">Keep Your Pet Happy</h1>
+                            <h5 class="text-white mb-3 d-none d-sm-block">Duo nonumy et dolor tempor no et. Diam sit diam sit diam erat</h5>
+                            <a href="" class="btn btn-lg btn-primary mt-3 mt-md-4 px-4">Book Now</a>
+                            <a href="" class="btn btn-lg btn-secondary mt-3 mt-md-4 px-4">Learn More</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="review-slider-item">
+                    <div class="review-img">
+                        <img src="img/review-1.jpg" alt="Image">
+                    </div>
+                    <div class="review-text">
+                        <h2>Customer Name</h2>
+                        <h3>Profession</h3>
+                        <div class="ratting">
+                            <i class="fa fa-star"></i>
+                            <i class="fa fa-star"></i>
+                            <i class="fa fa-star"></i>
+                            <i class="fa fa-star"></i>
+                            <i class="fa fa-star"></i>
+                        </div>
+                        <p>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur vitae nunc eget leo finibus luctus et vitae lorem
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-6">
+                <div class="review-slider-item">
+                    <div class="review-img">
+                        <img src="img/review-2.jpg" alt="Image">
+                    </div>
+                    <div class="review-text">
+                        <h2>Customer Name</h2>
+                        <h3>Profession</h3>
+                        <div class="ratting">
+                            <i class="fa fa-star"></i>
+                            <i class="fa fa-star"></i>
+                            <i class="fa fa-star"></i>
+                            <i class="fa fa-star"></i>
+                            <i class="fa fa-star"></i>
+                        </div>
+                        <p>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur vitae nunc eget leo finibus luctus et vitae lorem
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-6">
+                <div class="review-slider-item">
+                    <div class="review-img">
+                        <img src="img/review-3.jpg" alt="Image">
+                    </div>
+                    <div class="review-text">
+                        <h2>Customer Name</h2>
+                        <h3>Profession</h3>
+                        <div class="ratting">
+                            <i class="fa fa-star"></i>
+                            <i class="fa fa-star"></i>
+                            <i class="fa fa-star"></i>
+                            <i class="fa fa-star"></i>
+                            <i class="fa fa-star"></i>
+                        </div>
+                        <p>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur vitae nunc eget leo finibus luctus et vitae lorem
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    </div>
 
-    </head>
-
-    <body>
-
-@include('home._header')
-
-@include('home._contact')
+    </div>
+    </div>
+    </div>
+    <!-- Main Slider End -->
 
 
-@include('home._footer')
+    @include('home._contact')
+
+
+    @include('home._footer')
 
 
 </body>
