9a25f6e17ed95f0dc2d83a6c7ed46d1b0a049e52
Aynurbalci <64687379+Aynurbalci@users.noreply.github.com>
2022-05-11 23:25:37 +0300

    .

--- resources/views/admin/_sidebar.blade.php
+++ resources/views/admin/_sidebar.blade.php
@@ -1,79 +1,222 @@
-<nav class="sidebar sidebar-offcanvas" id="sidebar">
-    <ul class="nav">
-
-        <li class="nav-item nav-category">Menu</li>
+<div class="theme-setting-wrapper">
+    <div id="settings-trigger"><i class="ti-settings"></i></div>
+    <div id="theme-settings" class="settings-panel">
+        <i class="settings-close ti-close"></i>
+        <p class="settings-heading">SIDEBAR SKINS</p>
+        <div class="sidebar-bg-options selected" id="sidebar-light-theme"><div class="img-ss rounded-circle bg-light border me-3"></div>Light</div>
+        <div class="sidebar-bg-options" id="sidebar-dark-theme"><div class="img-ss rounded-circle bg-dark border me-3"></div>Dark</div>
+        <p class="settings-heading mt-2">HEADER SKINS</p>
+        <div class="color-tiles mx-0 px-4">
+            <div class="tiles success"></div>
+            <div class="tiles warning"></div>
+            <div class="tiles danger"></div>
+            <div class="tiles info"></div>
+            <div class="tiles dark"></div>
+            <div class="tiles default"></div>
+        </div>
+    </div>
+</div>
+<div id="right-sidebar" class="settings-panel">
+    <i class="settings-close ti-close"></i>
+    <ul class="nav nav-tabs border-top" id="setting-panel" role="tablist">
         <li class="nav-item">
-            <a href="/admin/users" class="nav-link" data-bs-toggle="collapse" href="#ui-basic" aria-expanded="false" aria-controls="ui-basic">
-                <i class="mdi mdi-account menu-icon"></i>
-                <span class="menu-title">User</span>
-
-
-            </a>
-
+            <a class="nav-link active" id="todo-tab" data-bs-toggle="tab" href="#todo-section" role="tab" aria-controls="todo-section" aria-expanded="true">TO DO LIST</a>
         </li>
         <li class="nav-item">
-            <a href="{{route('admin.category.index')}}" class="nav-link"  aria-expanded="false" aria-controls="ui-basic">
-                <i class="mdi mdi-format-list-bulleted-type menu-icon"></i>
-                <span class="menu-title">Category</span>
-
-            </a>
-
+            <a class="nav-link" id="chats-tab" data-bs-toggle="tab" href="#chats-section" role="tab" aria-controls="chats-section">CHATS</a>
         </li>
-        <li class="nav-item">
-            <a href="{{route('admin.treatment.index')}}" class="nav-link"  aria-expanded="false" aria-controls="ui-basic">
-                <i class="mdi mdi-seat-flat-angled menu-icon"></i>
-                <span class="menu-title">Treatments</span>
-                <i class=></i>
-            </a>
-
-        </li>
-        <li class="nav-item">
-            <a  class="nav-link"   aria-expanded="false" aria-controls="ui-basic">
-                <i class="mdi mdi-message-plus menu-icon"></i>
-                <span class="menu-title">Contact Message</span>
-                <i class=></i>
-            </a>
-
-        </li>
-        <li class="nav-item">
-            <a href="{{route('admin.category.index')}}" class="nav-link" data-bs-toggle="collapse" href="#ui-basic" aria-expanded="false" aria-controls="ui-basic">
-
-                <i class="mdi mdi-settings-box menu-icon">Settings</i>
-
-
-            </a>
-
-        </li>
-        <li class="nav-item">
-            <a href="/admin/faq" class="nav-link" data-bs-toggle="collapse" href="#ui-basic" aria-expanded="false" aria-controls="ui-basic">
-                <i class="dropdown-item-icon mdi mdi-help-circle-outline text-primary me-2 menu-icon"></i>
-                <span class="menu-title"> Faq</span>
-                <i class=></i>
-            </a>
-
-        </li>
-        <li class="nav-item">
-            <a href="/admin/reviews" class="nav-link" data-bs-toggle="collapse" href="#ui-basic" aria-expanded="false" aria-controls="ui-basic">
-                <i class="mdi mdi-clipboard-outline menu-icon"></i> <span class="menu-title">Reviews</span>
-                <i class=></i>
-            </a>
-
-        </li>
-
-        <li class="nav-item  @if(!@isset($page)) show-on-click @endif ">
-            <a class="nav-link" data-bs-toggle="collapse" href="#ui-basic" aria-expanded="false" aria-controls="ui-basic">
-                <i class="mdi mdi-heart-box menu-icon "></i>
-                <span class="menu-title">Randevus</span>
-                <i class="menu-arrow"></i>
-            </a>
-            <div class="collapse" id="ui-basic">
-                <ul class="nav flex-column sub-menu menu-icon">
-                    <li class="nav-item"> <a href="/admin/users" class="nav-link" href="pages/ui-features/buttons.html">Accepted appointment</a></li>
-                    <li class="nav-item"> <a href="/admin/users" class="nav-link" href="pages/ui-features/dropdowns.html">Cenceled appointment</a></li>
-                    <li class="nav-item"> <a href="/admin/users" class="nav-link" href="pages/ui-features/typography.html">Completed appointment</a></li>
+    </ul>
+    <div class="tab-content" id="setting-content">
+        <div class="tab-pane fade show active scroll-wrapper" id="todo-section" role="tabpanel" aria-labelledby="todo-section">
+            <div class="add-items d-flex px-3 mb-0">
+                <form class="form w-100">
+                    <div class="form-group d-flex">
+                        <input type="text" class="form-control todo-list-input" placeholder="Add To-do">
+                        <button type="submit" class="add btn btn-primary todo-list-add-btn" id="add-task">Add</button>
+                    </div>
+                </form>
+            </div>
+            <div class="list-wrapper px-3">
+                <ul class="d-flex flex-column-reverse todo-list">
+                    <li>
+                        <div class="form-check">
+                            <label class="form-check-label">
+                                <input class="checkbox" type="checkbox">
+                                Team review meeting at 3.00 PM
+                            </label>
+                        </div>
+                        <i class="remove ti-close"></i>
+                    </li>
+                    <li>
+                        <div class="form-check">
+                            <label class="form-check-label">
+                                <input class="checkbox" type="checkbox">
+                                Prepare for presentation
+                            </label>
+                        </div>
+                        <i class="remove ti-close"></i>
+                    </li>
+                    <li>
+                        <div class="form-check">
+                            <label class="form-check-label">
+                                <input class="checkbox" type="checkbox">
+                                Resolve all the low priority tickets due today
+                            </label>
+                        </div>
+                        <i class="remove ti-close"></i>
+                    </li>
+                    <li class="completed">
+                        <div class="form-check">
+                            <label class="form-check-label">
+                                <input class="checkbox" type="checkbox" checked>
+                                Schedule meeting for next week
+                            </label>
+                        </div>
+                        <i class="remove ti-close"></i>
+                    </li>
+                    <li class="completed">
+                        <div class="form-check">
+                            <label class="form-check-label">
+                                <input class="checkbox" type="checkbox" checked>
+                                Project review
+                            </label>
+                        </div>
+                        <i class="remove ti-close"></i>
+                    </li>
                 </ul>
             </div>
-        </li>
+            <h4 class="px-3 text-muted mt-5 fw-light mb-0">Events</h4>
+            <div class="events pt-4 px-3">
+                <div class="wrapper d-flex mb-2">
+                    <i class="ti-control-record text-primary me-2"></i>
+                    <span>Feb 11 2018</span>
+                </div>
+                <p class="mb-0 font-weight-thin text-gray">Creating component page build a js</p>
+                <p class="text-gray mb-0">The total number of sessions</p>
+            </div>
+            <div class="events pt-4 px-3">
+                <div class="wrapper d-flex mb-2">
+                    <i class="ti-control-record text-primary me-2"></i>
+                    <span>Feb 7 2018</span>
+                </div>
+                <p class="mb-0 font-weight-thin text-gray">Meeting with Alisa</p>
+                <p class="text-gray mb-0 ">Call Sarah Graves</p>
+            </div>
+        </div>
+        <!-- To do section tab ends -->
+        <div class="tab-pane fade" id="chats-section" role="tabpanel" aria-labelledby="chats-section">
+            <div class="d-flex align-items-center justify-content-between border-bottom">
+                <p class="settings-heading border-top-0 mb-3 pl-3 pt-0 border-bottom-0 pb-0">Friends</p>
+                <small class="settings-heading border-top-0 mb-3 pt-0 border-bottom-0 pb-0 pr-3 fw-normal">See All</small>
+            </div>
+            <ul class="chat-list">
+                <li class="list active">
+                    <div class="profile"><img src="{{asset('assets')}}/images/faces/face1.jpg" alt="image"><span class="online"></span></div>
+                    <div class="info">
+                        <p>Thomas Douglas</p>
+                        <p>Available</p>
+                    </div>
+                    <small class="text-muted my-auto">19 min</small>
+                </li>
+                <li class="list">
+                    <div class="profile"><img src="{{asset('assets')}}/images/faces/face2.jpg" alt="image"><span class="offline"></span></div>
+                    <div class="info">
+                        <div class="wrapper d-flex">
+                            <p>Catherine</p>
+                        </div>
+                        <p>Away</p>
+                    </div>
+                    <div class="badge badge-success badge-pill my-auto mx-2">4</div>
+                    <small class="text-muted my-auto">23 min</small>
+                </li>
+                <li class="list">
+                    <div class="profile"><img src="{{asset('assets')}}/images/faces/face3.jpg" alt="image"><span class="online"></span></div>
+                    <div class="info">
+                        <p>Daniel Russell</p>
+                        <p>Available</p>
+                    </div>
+                    <small class="text-muted my-auto">14 min</small>
+                </li>
+                <li class="list">
+                    <div class="profile"><img src="{{asset('assets')}}/images/faces/face4.jpg" alt="image"><span class="offline"></span></div>
+                    <div class="info">
+                        <p>James Richardson</p>
+                        <p>Away</p>
+                    </div>
+                    <small class="text-muted my-auto">2 min</small>
+                </li>
+                <li class="list">
+                    <div class="profile"><img src="{{asset('assets')}}/images/faces/face5.jpg" alt="image"><span class="online"></span></div>
+                    <div class="info">
+                        <p>Madeline Kennedy</p>
+                        <p>Available</p>
+                    </div>
+                    <small class="text-muted my-auto">5 min</small>
+                </li>
+                <li class="list">
+                    <div class="profile"><img src="{{asset('assets')}}/images/faces/face6.jpg" alt="image"><span class="online"></span></div>
+                    <div class="info">
+                        <p>Sarah Graves</p>
+                        <p>Available</p>
+                    </div>
+                    <small class="text-muted my-auto">47 min</small>
+                </li>
+            </ul>
+        </div>
+        <!-- chat tab ends -->
+    </div>
+</div>
+<!-- settings tab ends -->
 
+<nav class="sidebar sidebar-offcanvas" id="sidebar">
+    <ul class="nav">
+        <li class="nav-item" >
+            <a class="nav-link {{request()->routeIs('admin.index') ? 'active' : ''}}" href="{{route('admin.index')}}">
+                <i class="menu-icon mdi mdi-home"></i>
+                <span class="menu-title">Home</span>
+            </a>
+        </li>
+        <li class="nav-item {{request()->routeIs('admin.category.index') ? 'active' : ''}}">
+            <a class="nav-link" href="{{route('admin.category.index')}}">
+                <i class="menu-icon mdi mdi-buffer"></i>
+                <span class="menu-title">Category</span>
+            </a>
+        </li>
+        <li class="nav-item ">
+            <a class="nav-link {{request()->routeIs('/admin/treatment/') ? 'active' : ''}}" href="/admin/treatment">
+                <i class="menu-icon mdi mdi-file-multiple"></i>
+                <span class="menu-title">Treatment</span>
+            </a>
+        </li>
+        <li class="nav-item ">
+            <a class="nav-link" href="">
+                <i class="menu-icon mdi mdi-help"></i>
+                <span class="menu-title">FAQ</span>
+            </a>
+        </li>
+        <li class="nav-item ">
+            <a class="nav-link" href="">
+                <i class="menu-icon mdi mdi-help"></i>
+                <span class="menu-title">Documentation</span>
+            </a>
+        </li>
+        <li class="nav-item ">
+            <a class="nav-link" href="">
+                <i class="menu-icon mdi mdi-message-text-outline"></i>
+                <span class="menu-title">Comments</span>
+            </a>
+        </li>
+        <li class="nav-item ">
+            <a class="nav-link" href="">
+                <i class="menu-icon mdi mdi-account-multiple"></i>
+                <span class="menu-title">Users</span>
+            </a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="/admin/setting">
+                <i class="menu-icon mdi mdi-settings"></i>
+                <span class="menu-title">Settings</span>
+            </a>
+        </li>
     </ul>
 </nav>
