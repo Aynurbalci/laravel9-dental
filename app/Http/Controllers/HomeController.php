ddb6741848b8815c7397900bbbde725640ac9fe4
Aynurbalci <64687379+Aynurbalci@users.noreply.github.com>
2022-05-25 19:57:59 +0300

    .

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,9 @@
 namespace App\Http\Controllers;
 
 use App\Models\Category;
+
+use App\Models\Message;
+use App\Models\Setting;
 use App\Models\Treatment;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
@@ -12,8 +15,9 @@ class HomeController extends Controller
 {
 
 
-    public static function maincategorylist(){
-return Category::where('parent_id','=',11)->with('children')->get();
+    public static function maincategorylist()
+    {
+        return Category::where('parent_id', '=', 11)->with('children')->get();
     }
     /**
      * Handle an authentication attempt.
@@ -24,34 +28,81 @@ return Category::where('parent_id','=',11)->with('children')->get();
     public function Index()
     {
 
-        $sliderdata=Treatment::limit(4)->get();
-       $treatmentlist1=Treatment::limit(4)->get();
-        return view('home.index',[
+        $sliderdata = Treatment::limit(4)->get();
+        $treatmentlist1 = Treatment::limit(4)->get();
+        $setting = Setting::first();
 
+        return view('home.index', [
+            'setting' => $setting,
             'sliderdata' => $sliderdata,
-            'treatmentlist1'=>$treatmentlist1
+            'treatmentlist1' => $treatmentlist1
 
         ]);
     }
+    public function about()
+    {
+        $setting = Setting::first();
+        return view('home.about', [
+            'setting' => $setting,
+
+
+        ]);
+    }
+    public function references()
+    {
+
+
+        $setting = Setting::first();
+        return view('home.references', [
+            'setting' => $setting,
+
+        ]);
+    }
+    public function contact()
+    {
+
+
+        $setting = Setting::first();
+        return view('home.contact', [
+            'setting' => $setting,
+
+        ]);
+    }
+    public function storemessage(Request $request)
+    {
+
+        $data = new Message();
+        $data->name = $request->input('name');
+        $data->email = $request->input('email');
+        $data->phone = $request->input('phone');
+        $data->subject = $request->input('subject');
+        $data->message = $request->input('message');
+        $data->ip = request()->ip();
+        $data->save();
+        return redirect()->route('contact')->with('info', 'your message has been sent, Thank you.');
+    }
+
+
+
     public function treatment($id)
     {
-        $data=Treatment::find($id);
-        $images=DB::table('images')->where('treatment_id',$id)->get();
-              return view('home.treatment',[
+        $data = Treatment::find($id);
+        $images = DB::table('images')->where('treatment_id', $id)->get();
+        return view('home.treatment', [
             'data' => $data,
-            'images'=>$images
+            'images' => $images
 
         ]);
     }
-    public function categorytreatments($id,$slug)
+    public function categorytreatments($id, $slug)
     {
-        echo "asf";
+        echo "aynur";
         exit();
-        $data=Treatment::find($id);
-        $images=DB::table('images')->where('treatment_id',$id)->get();
-              return view('home.treatment',[
+        $data = Treatment::find($id);
+        $images = DB::table('images')->where('treatment_id', $id)->get();
+        return view('home.treatment', [
             'data' => $data,
-            'images'=>$images
+            'images' => $images
 
         ]);
     }
@@ -60,17 +111,17 @@ return Category::where('parent_id','=',11)->with('children')->get();
         $data['id'] = $id;
         $data['name'] = $name;
         return view('home.test', $data);
-
     }
     public function login()
-    {       return view('admin_login');
+    {
+        return view('admin_login');
     }
     public function logincheck(Request $request)
     {
 
 
         if ($request->isMethod('post')) {
-            $credentials = $request->only('email','password');
+            $credentials = $request->only('email', 'password');
             if (Auth::attempt($credentials)) {
                 $request->session()->regenerate();
                 return redirect()->intended(('admin'));
@@ -83,7 +134,8 @@ return Category::where('parent_id','=',11)->with('children')->get();
             return view('admin_login');
         }
     }
-    public function logout(Request $request){
+    public function logout(Request $request)
+    {
         Auth::logout();
         $request->session()->invalidate();
         $request->session()->regenerateToken();
